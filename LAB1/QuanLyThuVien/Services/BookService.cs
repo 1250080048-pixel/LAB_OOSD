53f5b3d523f0d371c5900c4ca0f6991a35ebb120
zatouu <kiritokirigaya498@gmail.com>
2026-09-18 21:40:27 +0700

    Dua code vao dung thu muc LAB1

---
+++ LAB1/QuanLyThuVien/Services/BookService.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using QuanLyThuVien.Data;
+
+namespace QuanLyThuVien.Services
+{
+    public class BookService
+    {
+        public DataTable GetAllBooks()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = DatabaseConnection.GetConnection())
+            {
+                string query = "SELECT * FROM Books";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+    }
+}
